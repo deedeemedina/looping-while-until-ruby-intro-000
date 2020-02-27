2fb20239efaff30cd89f384405f759d1b415d5e6
deedee medina <deedee.medinacorp@gmail.com>
2020-02-27 01:57:53 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,9 +2,9 @@
 using_while = 0
   while using_while < 10
   using_while += 1
-  puts "Wingardium Leviosa"
+  puts "Wingardium Leviosa #{using_while}"
 
   #your code here
 end
 
-puts "Wingardium Leviosa"
+puts "Wingardium Leviosa #{using_while}"
